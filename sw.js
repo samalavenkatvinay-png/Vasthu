dd44e05106c9cc5627d70d429f799640d04ec19c
samalavenkatvinay-png <samalavenkatvinay@gmail.com>
2026-03-21 23:30:07 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE = 'vastu-v3';
+
+self.addEventListener('install', () => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
